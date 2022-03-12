f04dd383d62689da99c912dbf7d3d0d69f11c5ac
Antizana <edwinvillacis@yahoo.com>
2022-03-12 17:16:51 -0500

    SpringBoot - React - AWS for my YouTube channel

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// SpringBoot - React - AWS for my YouTube channel
+import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
+import axios from "axios";
+import { useDropzone } from "react-dropzone";
+
+const UserProfiles = () => {
+  const [userProfiles, setUserProfiles] = useState([]);
+
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then((res) => {
+      console.log(res);
+      setUserProfiles(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return userProfiles.map((userProfile, index) => {
+    return (
+      <div key={index}>
+        {/* todo: profile image */}
+        <br />
+        <br />
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+        <Dropzone />
+        <br />
+      </div>
+    );
+  });
+};
+
+function Dropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the image here ...</p>
+      ) : (
+        <p>Drag 'n' drop profile image, or click to select profile image</p>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProfiles />
     </div>
   );
 }
